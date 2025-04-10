5ccfe4fab7727fc71dc57d0c1caba98f80212924
Kyle Swartz <kjswartz@github.com>
2025-04-10 14:38:41 -0400

    update repository to be slice to accept multiple repos at once

--- cmd/root.go
+++ cmd/root.go
@@ -3,8 +3,8 @@ package cmd
 import (
 	"bufio"
 	"fmt"
-
 	"os"
+	"strings"
 
 	"github.com/kjswartz/clark/pkg/githubclient"
 )
@@ -20,20 +20,31 @@ func RootCmd() {
 	// Collect data from the user
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("What is the name of the owner? ")
+	fmt.Print("What is the name for the owner? ")
 	owner, _ := reader.ReadString('\n')
 	owner = owner[:len(owner)-1] // Remove the newline character
 
-	fmt.Print("What is the name of the repository? ")
-	repo, _ := reader.ReadString('\n')
-	repo = repo[:len(repo)-1] // Remove the newline character
+	fmt.Print("What are the names of the repositories? ")
+	repoInput, _ := reader.ReadString('\n')
+	repoInput = repoInput[:len(repoInput)-1] // Remove the newline character
 
-	fmt.Printf("Owner: %s, Repository: %s\n", owner, repo)
-
-	err = gh.SubmitPullRequest(owner, repo)
-	if err != nil {
-		fmt.Println("Error submitting pull request:", err)
-		os.Exit(1)
+	// Split the input by "," and trim whitespace
+	repos := []string{}
+	for _, repo := range strings.Split(repoInput, ",") {
+		repos = append(repos, strings.TrimSpace(repo))
 	}
-	fmt.Println("Pull request submitted successfully!")
+
+	fmt.Printf("Owner: %s, Repositories: %v\n", owner, repos)
+
+	for _, repo := range repos {
+		fmt.Printf("Submitting pull request for %s/%s...\n", owner, repo)
+		// Submit the pull request
+		err = gh.SubmitPullRequest(owner, repo)
+		if err != nil {
+			fmt.Printf("Error submitting pull request for %s/%s: %v", owner, repo, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Pull request submitted successfully for %s/%s...\n", owner, repo)
+	}
 }
